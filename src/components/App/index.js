d12576a247452126045ef55b9785d6abda14dde5
Anton Konev <evilj0e@yandex-team.ru>
2017-03-02 18:21:46 +0500

    feat: initial project's config

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.css';
+
+const Hi = () => (
+    <div className={styles.app}>
+        <h2>Привет, ребята!</h2>
+    </div>
+);
+
+export default Hi;
